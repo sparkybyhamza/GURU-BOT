be740eb83a960981915b6d147d9c92edb0fadce5
sparkybyhamza <161278188+sparkybyhamza@users.noreply.github.com>
2024-02-26 11:34:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923217769365', 'sparky', true],
+  ['923217769365', 'sparky', true], 
   [''] 
 ] //Number of owners
 
